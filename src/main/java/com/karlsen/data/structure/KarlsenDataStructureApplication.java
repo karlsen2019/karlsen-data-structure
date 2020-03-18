9480af97e4b843bce774727410803f3ab319a0cc
Karlsen <jskarlsen@itcom888.com>
2020-03-19 05:37:43 +0800

    init repo

---
+++ src/main/java/com/karlsen/data/structure/KarlsenDataStructureApplication.java
@@ -0,0 +1,13 @@
+package com.karlsen.data.structure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarlsenDataStructureApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KarlsenDataStructureApplication.class, args);
+    }
+
+}
