52e696ac48609de2eac53d78b5a938415a0c49ba
Karlsen <jskarlsen@itcom888.com>
2020-03-20 03:09:10 +0800

    删除多余日志引用

--- src/main/java/com/karlsen/data/structure/stack/controller/StackController.java
+++ src/main/java/com/karlsen/data/structure/stack/controller/StackController.java
@@ -1,15 +1,16 @@
 package com.karlsen.data.structure.stack.controller;
 
 import com.karlsen.data.structure.stack.service.CheckCodeService;
-import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
+
 @RequestMapping("stack")
 @RestController
-@AllArgsConstructor
 public class StackController {
+    @Resource
     private CheckCodeService checkCodeService;
 
     @GetMapping("checkBrackets")
--- src/main/java/com/karlsen/data/structure/stack/service/CheckCodeService.java
+++ src/main/java/com/karlsen/data/structure/stack/service/CheckCodeService.java
@@ -1,10 +1,9 @@
 package com.karlsen.data.structure.stack.service;
 
 import com.karlsen.data.structure.stack.entity.Stack;
-import lombok.extern.slf4j.Slf4j;
+
 import org.springframework.stereotype.Service;
 
-@Slf4j
 @Service
 public class CheckCodeService {
     public boolean checkBrackets(String code) {
