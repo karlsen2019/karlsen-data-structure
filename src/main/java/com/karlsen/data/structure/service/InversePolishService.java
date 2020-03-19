0c6ba749b4a6d0160847cb7bfe02243bb87e6389
Karlsen <jskarlsen@itcom888.com>
2020-03-20 06:57:04 +0800

    完成逆波兰后缀表达式计算

--- src/main/java/com/karlsen/data/structure/service/InversePolishService.java
+++ src/main/java/com/karlsen/data/structure/service/InversePolishService.java
@@ -1,8 +1,15 @@
 package com.karlsen.data.structure.service;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.List;
+import java.util.Stack;
+
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
+
+import com.google.common.collect.Lists;
 
 import org.springframework.stereotype.Service;
 
@@ -23,11 +30,28 @@ public class InversePolishService {
 
     public String eval(String expression) {
         try {
-            return String.valueOf(scriptEngine.eval(expression));
+            BigDecimal result = new BigDecimal(scriptEngine.eval(expression).toString());
+            return result.setScale(0, RoundingMode.DOWN).toString();
         } catch (ScriptException e) {
             log.error(e.getMessage(), e);
         }
         return "表达式错误";
+    }
+
+    public String inversePolish(List<String> items) {
+        List<String> operations = Lists.newArrayList("+", "-", "*", "/");
+        Stack<String> inversePolishStack = new Stack<>();
+        for (String item : items) {
+            if (!operations.contains(item)) {
+                inversePolishStack.push(item);
+            } else {
+                String first = inversePolishStack.pop();
+                String second = inversePolishStack.pop();
+                String expression = second + item + first;
+                inversePolishStack.push(eval(expression));
+            }
+        }
+        return inversePolishStack.pop();
     }
 
 }
--- src/test/java/com/karlsen/data/structure/service/InversePolishServiceTest.java
+++ src/test/java/com/karlsen/data/structure/service/InversePolishServiceTest.java
@@ -2,16 +2,23 @@ package com.karlsen.data.structure.service;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.List;
+
 import javax.annotation.Resource;
+
+import com.google.common.collect.Lists;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import lombok.extern.slf4j.Slf4j;
+
 /**
  * InversePolishServiceTest
  */
+@Slf4j
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class InversePolishServiceTest {
@@ -27,5 +34,13 @@ public class InversePolishServiceTest {
         assertTrue(inversePolishService.eval(expresion1).equals("48"));
         assertTrue(inversePolishService.eval(expresion2).equals("48"));
         assertTrue(inversePolishService.eval(expresion3).equals("32"));
+    }
+
+    @Test
+    public void inversePolish() {
+        List<String> list = Lists.newArrayList("4", "13", "5", "/", "+");
+        String reslult = inversePolishService.inversePolish(list);
+        log.info("后缀表达式结果为：{}", reslult);
+        assertTrue("6".equals(reslult));
     }
 }
